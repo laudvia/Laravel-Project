a9ae835a3638d57150ee9adb951461b7e5c34100
laudvia <cycycyyuigv@yandex.ru>
2025-11-13 17:11:59 +0300

    lr2

--- resources/views/main/hello.blade.php
+++ resources/views/main/hello.blade.php
@@ -1,7 +1,25 @@
 @extends('layout')
 @section('content')
-    <h2>Hello! I'm your first laravel project</h2>
-@endsection
-@section('name')
-    <h2>Шмелев Андрей Павлович 231-322</h2>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Name</th>
+      <th scope="col">shortDesc</th>
+      <th scope="col">Desc</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($articles as $article)
+    <tr>
+      <th scope="row">{{$article['date']}}</th>
+      <td>{{$article['name']}}</td>
+      <td>{{$article['shortDesc']}}</td>
+      <td>{{$article['desc']}}</td>
+      <td><a href="/galery/{{$article['full_image']}}"><img src="{{URL::asset('/images/'.$article['preview_image'])}}" alt="" height="100" width="100"></a></td>
+</tr>
+@endforeach
+  </tbody>
+</table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main/hello');
-});
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/galery/{full_image}', [MainController::class, 'show']);
 
 Route::get('/about', function () {
     return view('main/about');
